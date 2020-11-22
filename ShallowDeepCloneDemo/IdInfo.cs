77a0067a5cd5ba2266069bae56d3a5c280fd1b52
NaraaB <sanchir65@gmail.com>
2020-11-22 02:17:42 +0100

    Add project files.

---
+++ ShallowDeepCloneDemo/IdInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShallowDeepCloneDemo
+{
+    class IdInfo
+    {
+        public int IdNumber;
+        public IdInfo(int idNumber)
+        {
+            this.IdNumber = idNumber;
+        }
+    }
+}
